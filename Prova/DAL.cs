d153044a7cbe583ba22ac7495ce637eb1eed48cb
michelmlg <147891522+michelmlg@users.noreply.github.com>
2024-07-18 23:30:33 -0300

    InitialCommit

---
+++ Prova/DAL.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace Prova
+{
+    class DAL
+    {
+        private static String strConexao = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=BDFarinha.mdb";
+        private static OleDbConnection conn = new OleDbConnection(strConexao);
+        private static OleDbCommand strSQL;
+        private static OleDbDataReader result;
+
+        public static void conecta()
+        {
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception)
+            {
+                Erro.setMsg("Problemas ao se conectar ao Banco de Dados");
+            }
+
+        }
+
+        public static void desconecta()
+        {
+            conn.Close();
+        }
+
+        public static void consultaUmCliente()
+        {
+            String aux = "select * from TabClientes where cnpj = '" + Cliente.getCNPJ() + "'";
+
+            strSQL = new OleDbCommand(aux, conn);
+            result = strSQL.ExecuteReader();
+            Erro.setErro(false);
+            if (result.Read())
+            {
+                Cliente.setNome(result.GetString(1));
+            }
+            else
+                Erro.setMsg("Cliente não cadastrado.");
+        }
+
+        public static void populaVendaDR()
+        {
+            String aux = "select * from TabVendasCliente where cnpj = '" + Cliente.getCNPJ() + "'";
+
+            strSQL = new OleDbCommand(aux, conn);
+            result = strSQL.ExecuteReader();
+        }
+
+        public static void getProximaVenda()
+        {
+            Erro.setErro(false);
+            if (result.Read())
+            {
+                VendaCliente.setCodigo(result.GetValue(0).ToString());
+                VendaCliente.setCNPJ(result.GetString(1));
+                VendaCliente.setData(result.GetString(2));
+                VendaCliente.setToneladas(result.GetString(3));
+                VendaCliente.setvalor(result.GetString(4));
+            }
+            else
+                Erro.setErro(true);
+            
+        }
+    }
+}
---
+++ Prova/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prova
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BLL.conecta();
+            if (Erro.getErro())
+                MessageBox.Show(Erro.getMsg());
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            BLL.desconecta();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cliente.setCNPJ(textBox1.Text);
+
+            BLL.validaCNPJ();
+
+            if(Erro.getErro())
+                MessageBox.Show(Erro.getMsg());
+            else
+            {
+                textBox2.Text = Cliente.getNome();
+
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                listBox3.Items.Clear();
+
+                double somaToneladas = 0;
+                double somaValores = 0;
+
+                BLL.getProximaVenda(); 
+                while (!Erro.getErro())
+                {
+                    string toneladasStr = VendaCliente.getToneladas();
+                    double toneladasDbl = Double.Parse(toneladasStr);
+                    somaToneladas += toneladasDbl;
+
+                    string valoresStr = VendaCliente.getValor();
+                    double valoresDbl = Double.Parse(valoresStr);
+                    somaValores += valoresDbl;
+
+                    listBox1.Items.Add(VendaCliente.getData());
+                    listBox2.Items.Add(toneladasStr);
+                    listBox3.Items.Add(valoresStr);
+
+                    BLL.getProximaVenda();
+                }
+
+                textBox3.Text = somaToneladas.ToString();
+                textBox4.Text = somaValores.ToString();
+            }
+        }
+    }
+}
